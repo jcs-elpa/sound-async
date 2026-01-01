bf25ff858284cb232f704b67c3cbac127c2907a8
JenChieh <jcs090218@gmail.com>
2026-01-01 14:02:58 +0800

    chore: Update copyright year

--- sound-async.el
+++ sound-async.el
@@ -1,6 +1,6 @@
 ;;; sound-async.el --- Play sound asynchronously  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2025  Shen, Jen-Chieh
+;; Copyright (C) 2022-2026  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
